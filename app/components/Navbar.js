c0d9bc0feda81a209e41c2af7293b5ff6d65a4e7
Bravo1B9 <b1@xgmi.com>
2022-09-06 04:56:49 +0000

    Create signin

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -3,7 +3,6 @@ import {
   Box,
   Flex,
   Avatar,
-  Link,
   Button,
   Menu,
   MenuButton,
@@ -17,8 +16,76 @@ import {
 } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
+import { useState, useEffect } from 'react';
+import { ethers, providers } from 'ethers';
+import { createClient, STORAGE_KEY, authenticate as authenticateMutation, getChallenge, getDefaultProfile } from '../api';
+import { parseJwt, refreshAuthToken } from '../utils';
+
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [connected, setConnected] = useState(true)
+  const [userAddress, setUserAddress] = useState()
+  const [userProfile, setUserProfile] = useState()
+
+  useEffect(() => {
+    refreshAuthToken()
+    async function checkConnection() {
+      const provider = new ethers.providers.Web3Provider(
+        (window).ethereum
+      )
+      const addresses = await provider.listAccounts();
+      if (addresses.length) {
+        setConnected(true)
+        setUserAddress(addresses[0])
+        getUserProfile(addresses[0])
+      } else {
+        setConnected(false)
+      }
+    }
+    checkConnection()
+  }, [])
+
+  async function getUserProfile(address) {
+    try {
+      const urqlClient = await createClient()
+      const response = await urqlClient.query(getDefaultProfile, {
+        address
+      }).toPromise()
+      console.log({response})
+      setUserProfile(response.data.defaultProfile)
+    } catch (err) {
+      console.log('error fetching user profile...: ', err)
+    }
+  }
+
+  async function signIn() {
+    try {
+      const accounts = await window.ethereum.send(
+        "eth_requestAccounts"
+      )
+      setConnected(true)
+      const account = accounts.result[0]
+      setUserAddress(account)
+      const urqlClient = await createClient()
+      const response = await urqlClient.query(getChallenge, {
+        address: account
+      }).toPromise()
+      const provider = new providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const signature = await signer.signMessage(response.data.challenge.text)
+      const authData = await urqlClient.mutation(authenticateMutation, {
+        address: account, signature
+      }).toPromise()
+      const { accessToken, refreshToken } = authData.data.authenticate
+      const accessTokenData = parseJwt(accessToken)
+      getUserProfile(account)
+      localStorage.setItem(STORAGE_KEY, JSON.stringify({
+        accessToken, refreshToken, exp: accessTokenData.exp
+      }))
+    } catch (err) {
+      console.log('error: ', err)
+    }
+  }
 
   return (
     <>
@@ -62,7 +129,7 @@ export default function Navbar() {
                   <MenuItem>Account Settings</MenuItem>
                   <MenuItem>Logout</MenuItem>
                 </MenuList>
-                <Button>Login</Button>
+                <Button onClick={signIn}>Login</Button>
               </Menu>
             </Stack>
           </Flex>
